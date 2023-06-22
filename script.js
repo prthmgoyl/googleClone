fa45e3737c882f5805c7fb15003d4920ed5dede5
prthmgoyl <prthmgoyl@gmail.com>
2023-06-22 16:23:24 +0530

    commitjs

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBuY0FJeGUZav4MK4kv8_Ok4sDb_2McY_M",
-    authDomain: "clone-fce8c.firebaseapp.com",
-    projectId: "clone-fce8c",
-    storageBucket: "clone-fce8c.appspot.com",
-    messagingSenderId: "554136031261",
-    appId: "1:554136031261:web:a340356c329787410436f4",
-    measurementId: "G-KNR6YFJ2J7"
-
+    apiKey: "AIzaSyDREMNISyygJJLyNW9nDvrxnpCUus7zegQ",
+    authDomain: "clone-fc6b2.firebaseapp.com",
+    projectId: "clone-fc6b2",
+    storageBucket: "clone-fc6b2.appspot.com",
+    messagingSenderId: "554041088263",
+    appId: "1:554041088263:web:a62db0115517496fe18da2",
+    measurementId: "G-7W23V7WJCT"
   };
+
 
 firebase.initializeApp(firebaseConfig);
 
